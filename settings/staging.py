eeea63c491a91d4d04480462d0e03483d1de2c9a
kaiforward <kaiforward123@gmail.com>
2018-02-04 19:21:43 +0000

    fixed staging file

--- settings/staging.py
+++ settings/staging.py
@@ -8,3 +8,22 @@ DEBUG = False
 DATABASES = {
     'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
+
+SITE_URL = 'https://steering-space.herokuapp.com'
+ALLOWED_HOSTS.append('steering-space.herokuapp.com')
+# Log DEBUG information to the console
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
